67cd78cac7e0efcfd0f12335db60bc9396977640
kawsar-pie <79654190+kawsar-pie@users.noreply.github.com>
2023-03-07 21:22:53 +0600

    news_category_items msg added

--- app.js
+++ app.js
@@ -7,8 +7,22 @@ const displayNewsCategory = data =>{
     const newsCategoryField = document.getElementById("news_category_field");
     for(let i=0;i<data.news_category.length;i++){
         // console.log(data.news_category[i].category_name);
-        const newCategory = documrnt.createElement
+        const newCategory = document.createElement('div');
+        newCategory.innerHTML=`
+        <button id="${data.news_category[i].category_id}"class="btn btn-secondary">${data.news_category[i].category_name}</button>
+        `
+        newsCategoryField.appendChild(newCategory)
     }
 }
+
+document.getElementById("news_category_field").addEventListener("click", function(event) {
+    var clickedElement = event.target;
+    var clickedElementId = clickedElement.getAttribute("id");
+    // console.log("Clicked element ID: " + clickedElementId);
+    if(clickedElementId==="news_category_field") return;
+    loadClickedNews(clickedElementId,event.target.innerText);
+    console.log(clickedElementId,event.target.innerText);
+});
+
 
 loadNewsCategory();
