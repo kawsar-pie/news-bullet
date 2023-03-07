640820a1a4d6f33ade5aff1cc49c5005ef523f3f
kawsar-pie <79654190+kawsar-pie@users.noreply.github.com>
2023-03-07 21:24:38 +0600

    news category details  added

--- app.js
+++ app.js
@@ -24,5 +24,36 @@ document.getElementById("news_category_field").addEventListener("click", functio
     console.log(clickedElementId,event.target.innerText);
 });
 
-
+const loadClickedNews=(id,category_name)=>{
+    const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
+    fetch(url).then(response=>response.json()).then(data =>showClickedNews(data.data,category_name));
+}
+const showClickedNews = (data,category_name) =>{
+    const newsCategoryFieldMsg = document.getElementById("news_category_field_msg");
+    newsCategoryFieldMsg.innerHTML=`
+        <p class="bg-white border rounded-2 px-2 py-2">${data.length} items found for category <span class="text-success fs-5">${category_name}</span></p>
+        `
+    const newsDetailsField = document.getElementById("news_details_field");
+    for(let i=0;i<data.length;i++){
+        // console.log(data.news_category[i].category_name);
+        const newNews = document.createElement('div');
+        newNews.innerHTML=`
+        <div class="card shadow m-5" style="max-width: 95%;">
+            <div class="row g-0">
+              <div class="col-md-5">
+                <img src="${data[i].thumbnail_url}" class="img-fluid p-3 rounded-start" alt="...">
+              </div>
+              <div class="col-md-7">
+                <div class="card-body">
+                  <b><h5 class="card-title">${data[i].title}</h5></b>
+                  <p class="card-text">${data[i].details}</p>
+                  <p class="card-text"><small class="text-muted">${data[i].author.published_date}</small></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        `
+        newsDetailsField.appendChild(newNews)
+    }
+}
 loadNewsCategory();
