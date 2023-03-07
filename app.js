a11ec92b9933526281598bfe7509961b5eb49df6
kawsar-pie <79654190+kawsar-pie@users.noreply.github.com>
2023-03-07 22:01:22 +0600

    some error handled

--- app.js
+++ app.js
@@ -8,19 +8,32 @@ const displayNewsCategory = data =>{
     for(let i=0;i<data.news_category.length;i++){
         // console.log(data.news_category[i].category_name);
         const newCategory = document.createElement('div');
-        newCategory.innerHTML=`
+
+        if(i==data.news_category.length-1){
+            newCategory.innerHTML=`
+            <button id="${data.news_category[i].category_id}"class="btn btn-info">${data.news_category[i].category_name}</button>
+            `
+        }
+        else{
+            newCategory.innerHTML=`
         <button id="${data.news_category[i].category_id}"class="btn btn-secondary">${data.news_category[i].category_name}</button>
         `
+        }
+        
         newsCategoryField.appendChild(newCategory)
     }
 }
+
 
 document.getElementById("news_category_field").addEventListener("click", function(event) {
     var clickedElement = event.target;
     var clickedElementId = clickedElement.getAttribute("id");
     // console.log("Clicked element ID: " + clickedElementId);
     if(clickedElementId==="news_category_field") return;
+    const markClicked = document.getElementById(clickedElementId);
+    markClicked.classList.add("bg-info");
     loadClickedNews(clickedElementId,event.target.innerText);
+    // markClicked.classList.remove("bg-info");
     console.log(clickedElementId,event.target.innerText);
 });
 
@@ -34,6 +47,7 @@ const showClickedNews = (data,category_name) =>{
         <p class="bg-white border rounded-2 px-2 py-2">${data.length} items found for category <span class="text-success fs-5">${category_name}</span></p>
         `
     const newsDetailsField = document.getElementById("news_details_field");
+    newsDetailsField.innerHTML = ``;
     for(let i=0;i<data.length;i++){
         // console.log(data.news_category[i].category_name);
         const newNews = document.createElement('div');
@@ -46,7 +60,7 @@ const showClickedNews = (data,category_name) =>{
               <div class="col-md-7">
                 <div class="card-body">
                   <b><h5 class="card-title">${data[i].title}</h5></b>
-                  <p class="card-text">${data[i].details}</p>
+                  <p class="card-text">${data[i].details.slice(0,500)}...</p>
                   <p class="card-text"><small class="text-muted">${data[i].author.published_date}</small></p>
                 </div>
               </div>
@@ -56,4 +70,5 @@ const showClickedNews = (data,category_name) =>{
         newsDetailsField.appendChild(newNews)
     }
 }
+loadClickedNews('08',"All News")
 loadNewsCategory();
